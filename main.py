af00c764a48f2f5cc17d71709061c1052a80a485
xb0yx2k20 <musraul@yandex.ru>
2024-02-16 01:51:19 +0300

    clickSearch

--- main.py
+++ main.py
@@ -1,15 +1,46 @@
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+import re
 
-# Создайте объект ChromeOptions
-chrome_options = webdriver.ChromeOptions()
+service = Service(executable_path=ChromeDriverManager().install())
 
-# Укажите путь к драйверу в ChromeOptions
-chrome_options.add_argument('--chromedriver=/Users/xb0yx2k20/Downloads/chromedriver_mac_arm64/chromedriver')
+driver = webdriver.Chrome(service=service)
 
-# Добавьте любые другие необходимые опции
-# Например, отключение отображения изображений
-chrome_options.add_argument('--blink-settings=imagesEnabled=false')
+url1 = 'https://galaxystore.ru/about/contacts/'
+url2 = "https://repetitors.info"
+url3 = "https://hands.ru/company/about/"
+urls = [url1, url2, url3]
+for url in urls:
+    driver.get(url)
+    source = driver.page_source
 
-# Используйте объект ChromeOptions при создании объекта WebDriver
-driver = webdriver.Chrome(options=chrome_options)
+    print("URL:", url)
 
+    all_phones = []
+
+
+    # Regex search - not ideal pattern
+    phone_regex = r'\b8\s?\(\d{3}\)\s?\d{3}-\d{2}-\d{2}\b'
+    phone_match = re.findall(phone_regex, source)
+    # print(phone_match)
+
+    # XPath search
+    phones = driver.find_elements("xpath", "//a [starts-with (@href, 'tel:')]")
+    for phone in phones:
+        print(phone.text)
+
+    # Click search
+    try:
+        hidden_phones = driver.find_elements("xpath", "//* [contains (@class, 'phone') and contains (@class, 'link')]")
+        for phone in hidden_phones:
+            if phone not in phones and phone not in phone_match:
+                match = re.match(phone_regex, phone)
+                if (match):
+                    all_phones.append(phone)
+                    print(phone)
+                else:
+                    # click
+        #print(hidden_phones.text)
+    except:
+        print("Hidden phones not found")
--- test.py
+++ test.py
@@ -9,18 +9,24 @@ driver = webdriver.Chrome(service=service)
 
 url1 = 'https://galaxystore.ru/about/contacts/'
 url2 = "https://repetitors.info"
-driver.get(url1)
+mas = []
+mas.append(url1)
+mas.append(url2)
 
-source = driver.page_source
+for i in range(2):
 
-# Regex search - not ideal pattern
-phone_pattern = r'\b8\s?\(\d{3}\)\s?\d{3}-\d{2}-\d{2}\b'
-phone_match = re.findall(phone_pattern, source)
-# print(phone_match)
+    driver.get(mas[i])
 
-# XPath search
-phones = driver.find_elements("xpath", "//a [starts-with (@href, 'tel:')]")
-for phone in phones:
-    print(phone.text)
+    source = driver.page_source
 
-# Click search
+    # Regex search - not ideal pattern
+    phone_pattern = r'\b8\s?\(\d{3}\)\s?\d{3}-\d{2}-\d{2}\b'
+    phone_match = re.findall(phone_pattern, source)
+    # print(phone_match)
+
+    # XPath search
+    phones = driver.find_elements("xpath", "//a [starts-with (@href, 'tel:')]")
+    for phone in phones:
+        print(phone.text)
+
+    # Click search
